a05d946a55444bb45ed5cb860570f12907ef3a9b
perkynana <perkins.matthew.86@gmail.com>
2022-10-10 23:16:43 +1300

    initial js

---
+++ main.js
@@ -0,0 +1,13 @@
+// Assigning both User and Computer Scores to be updated
+const userScore = document.querySelector('.userScore')
+const computerScore = document.querySelector('.computerScore')
+
+// Assigning User Selection
+const userRock = document.querySelector('.userRock');
+const userPaper = document.querySelector('.userPaper');
+const userScissors = document.querySelector('.userScissors');
+
+// Assigning Computer Selection
+const computerRock = document.querySelector('.computerRock');
+const computerPaper = document.querySelector('.computerPaper');
+const computerScissors = document.querySelector('.computerScissors');
